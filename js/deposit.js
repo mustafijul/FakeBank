3e681457fb58b66c925965c22f49fe1e118182f1
mustafij <mustafijulislam21@gmail.com>
2024-06-16 14:55:05 +0600

    deposit calculatio done using event handler

--- js/deposit.js
+++ js/deposit.js
@@ -2,11 +2,15 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
 //   console.log("deposit button clicked");
 const depositField = document.getElementById("Deposit-id");
-const depositValue = depositField.value;
+const depositValue = parseInt(depositField.value);
 console.log(depositValue);
 
 const depoTotal = document.getElementById("deposit-total");
-depoTotal.innerText = depositValue;
+// console.log();
+const previousDepoTotal = parseInt(depoTotal.innerText);
+
+console.log(previousDepoTotal);
+depoTotal.innerText = depositValue + previousDepoTotal;
 });
 
 // withdraw btn
