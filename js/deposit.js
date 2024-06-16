be2f2696b74ea9200ca8545181de309a4bfb579c
mustafij <mustafijulislam21@gmail.com>
2024-06-16 13:09:31 +0600

    deposit section updated

--- js/deposit.js
+++ js/deposit.js
@@ -1,13 +1,15 @@
 // console.log("console from deposit");
-document.getElementById("btn-deposit").addEventListener('click', function(){
-    console.log("deposit button clicked");
-})
+document.getElementById("btn-deposit").addEventListener("click", function () {
+//   console.log("deposit button clicked");
+const depositField = document.getElementById("Deposit-id");
+const depositValue = depositField.value;
+console.log(depositValue);
 
-
-
-
+const depoTotal = document.getElementById("deposit-total");
+depoTotal.innerText = depositValue;
+});
 
 // withdraw btn
-document.getElementById("btn-withdraw").addEventListener('click', function(){
-    console.log("Withdraw button clicked");
-})
+// document.getElementById("btn-withdraw").addEventListener("click", function () {
+//   console.log("Withdraw button clicked");
+// });
